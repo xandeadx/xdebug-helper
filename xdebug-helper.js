31469a2aac3a5a28b6979d954272e390956ec8f2
xandeadx <xxandeadxx@gmail.com>
2018-02-02 18:00:05 +0300

    first commit

---
+++ xdebug-helper.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         xDebug helper
+// @namespace    http://xandeadx.ru/
+// @version      0.1
+// @author       xandeadx
+// @match        http://*.local/*
+// @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.2.0/js.cookie.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log(Cookies);
+    document.addEventListener('keydown', function(event) {
+        if (event.key == 'F5' && event.ctrlKey) {
+            var currentDate = new Date();
+            Cookies.set('XDEBUG_SESSION', 'PHPSTORM', {expires: new Date(currentDate.getTime() + 1000)});
+        }
+    });
+})();
